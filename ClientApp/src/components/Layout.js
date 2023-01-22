a2bc7ca3e21e12106a6e3d1feda08362b1dc958f
SebasMet <92382477+SebasMet@users.noreply.github.com>
2023-01-22 05:28:28 +0100

    Add homepage

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
+import './Layout.css'
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -8,8 +9,8 @@ export class Layout extends Component {
   render() {
     return (
       <div>
-        <NavMenu />
-        <Container tag="main">
+        {/* <NavMenu /> */}
+        <Container fluid tag="main">
           {this.props.children}
         </Container>
       </div>
--- ClientApp/src/components/Web/Home.js
+++ ClientApp/src/components/Web/Home.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import './Assets/Home.css';
+import { NavMenu } from '../NavMenu';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -6,20 +11,97 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+
+        <div className="image-container">
+          <img id="main-image" src={require("./Media/v7_6.png")} />
+          <div class="centered">Theater Laak</div>
+        </div>
+
+        <hr id="hr1"></hr>
+        <NavMenu />
+
+
+        <div id="text-container-1">
+          <h1 id="head-1"> Rondleiding </h1>
+        </div>
+
+        <div id='section-1' className='flexbox'>
+          <div className='section-block'>
+            <img className='section-image' src={require("./Media/image2.png")} />
+          </div>
+          <div className='section-block'>
+            <h3>
+              Wilt u alle actuele voorstellingen bekijken, klik dan op agenda.
+            </h3>
+            <button></button>
+          </div>
+        </div>
+
+        <div id='section-2' className='flexbox'>
+          <div className='section-block'>
+            <h3>
+              Wilt u tickets bestellen voor een voorstelling, klik dan op de tickets button om gemakkelijk naar de tickets pagina te gaan.
+            </h3>
+          </div>
+          <div className='section-block'>
+            <img className='section-image' src={require("./Media/image3.png")} />
+          </div>
+        </div>
+
+        <div id='section-3' className='flexbox'>
+          <div className='section-block'>
+            <img className='section-image' src={require("./Media/v154_201.png")} />
+          </div>
+          <div className='section-block'>
+            <h3>
+              Wilt u een zaal huren (of meerdere zalen) voor bijvoorbeeld een evenement of een ceremonie, klik dan op de Huren button om gemakkelijk zalen en of ruimtes te kunnen huren.
+            </h3>
+          </div>
+        </div>
+
+        <div id='section-4' className='flexbox'>
+          <div className='section-block'>
+            <h3>
+              Wilt u een bedrag doneren aan Laak Theater, klik dan op de knop donaties.
+            </h3>
+          </div>
+          <div className='section-block'>
+            <img className='section-image' src={require("./Media/v154_203.png")} />
+          </div>
+        </div>
+
+        <div id='section-5' className='flexbox'>
+          <div className='section-block'>
+            <img className='section-image' src={require("./Media/v154_205.png")} />
+          </div>
+          <div className='section-block'>
+            <h3>
+              Wilt u meer weten over Laak Theater, klik dan op over ons knop om naar de pagina te kunnen gaan.
+            </h3>
+          </div>
+        </div>
+
+        <div id='section-6' className='flexbox'>
+          <div className='section-block'>
+            <h3>
+              Wilt u ons contacteren, klik dan op contact om een contactformulier te kunnen invullen en of contactgegevens over ons te raadplegen            </h3>
+          </div>
+          <div className='section-block'>
+            <img className='section-image' src={require("./Media/v167_160.png")} />
+          </div>
+        </div>
+
+        <div id='section-7' className='flexbox'>
+          <div className='section-block'>
+            <img className='section-image' src={require("./Media/v169_162.png")} />
+          </div>
+          <div className='section-block'>
+            <h3>
+              Wilt u een eigen account aanmaken of inloggen ga dan naar Mijn Laak.
+            </h3>
+          </div>
+        </div>
+
       </div>
     );
   }
